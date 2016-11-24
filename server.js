039aa821380d6c14bde212187fab459c3352d557
Wing Liang <airwing628@gmail.com>
2016-11-23 21:32:37 -0800

    prepped repo for heroku

--- server.js
+++ server.js
@@ -3,9 +3,21 @@ var express = require('express');
 // create our app
 var app = express();
 
+// PORT for Heroku OR 3000 for localhost
+const PORT = process.env.PORT || 3000;
+
+// free version of openWeatherMap API only operates on http, so redirect all https traffic to http
+app.use(function (req, res, next){
+	if (req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	} else {
+		res.redirect('http://' + req.hostname + req.url);
+	}
+})
+
 // tell app which folder to serve
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-	console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+	console.log('Express server is up on port ' + PORT);
 });
